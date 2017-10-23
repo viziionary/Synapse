4a5f65511790fc48fb66c7eaf2e32f367dd6b985
viziionary <jonathan_t0dd@yahoo.com>
2017-10-23 15:51:25 -0500

    Cuts

---
+++ functions/getlayersize.js
@@ -0,0 +1,4 @@
+function getLayerSize(brain, layer) {
+    var keys = Object.keys(brain.globalReferenceNeurons).filter(k => brain.globalReferenceNeurons[k].layer === layer);
+    return keys.length;
+}
