f6fb555b0b19e9b4b46718752f805c7077b63e0a
viziionary <jonathan_t0dd@yahoo.com>
2017-10-23 16:24:40 -0500

    example format

---
+++ examples/example1/app-new.js
@@ -0,0 +1,8 @@
+var entity = new Brain(20, 2, 3);
+entity.evolve(function(entity){
+  // run user-designed simulation code
+  entity.input([1, 0.5, 0.2, 0, 1 /* ... */ ]);
+  var output = entity.output;
+  var currentScore = entity.score;
+  entity.score = 100;
+});
