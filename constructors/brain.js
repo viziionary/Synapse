1a459d659e7c349e1f271388b89b099fc0310542
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-23 14:52:43 -0600

    cleanup brain

--- constructors/brain.js
+++ constructors/brain.js
@@ -1,4 +1,5 @@
 const Neuron = require('./neuron');
+
 function Brain(inputSize, outputSize, layers) {
   var that = this;
   if (!layers) {
@@ -22,63 +23,23 @@ function Brain(inputSize, outputSize, layers) {
 
   this.input = (array) => {
     var inputs = [];
-    //console.log(that.globalReferenceNeurons);
     Object.values(this.globalReferenceNeurons).forEach(neuron => {
       if (neuron.layer == 0) {
         inputs.push(neuron);
       }
     });
-    //             for (prop1 in this.globalReferenceNeurons) {
-    //                 if (this.globalReferenceNeurons[prop1].layer == 0) {
-    //                     inputs.push(this.globalReferenceNeurons[prop1]);
-    //                 }
-    //             }
-    //console.log('Input size: ' + inputs.length);
     inputs.forEach((input, index) => {
       input.transmit(array[index]);
     });
-    //             for (var i1 = 0; i1 < inputs.length; i1++) {
-    //                 inputs[i1].transmit(array[i1]);
-    //             }
-
-    //             for (var i1 = 0; i1 < outputSize; i1++) { /* FUCK UP */
-    //console.log(this.brain[this.brain.length - 1][i2]);
     return Object.values(this.globalReferenceNeurons).filter(neuron => {
       return neuron.layer == layers - 1
     }).map(neuron => {
       return neuron.measure();
     });
-    //                 for (prop1 in this.globalReferenceNeurons) {
-    //                     if (this.globalReferenceNeurons[prop1].layer == layers - 1) {
-    //                         output.push(this.globalReferenceNeurons[prop1].measure());
-    //                     }
-    //                 }
-
-    //}
   };
 
-  //this.mutate = function() {
-  //    for (var i1 = 0; i1 < Math.floor(this.mutationRate); i1++) {
-  //        var mutationsList = [];
-  //        for (var name in this.mutations) {
-  //            for (var i2 = 0; i2 < this.mutations[name].frequency; i2++) {
-  //                mutationsList.push(this.mutations[name]);
-  //            }
-  //        }
-  //        mutationsList[getRandomInt(0, mutationsList.length - 1)].mutate(that);
-  //    }
-  //}
-
   this.generate = function() {
-    //console.log('Mutating...');
-    //console.time('generation');
-    //console.time('extension');
     that.activations = 0;
-    //var child = clone(this, true);
-    //console.timeEnd('extension');
-    //console.time('selection');
-    //console.timeEnd('selection');
-    //console.time('mutation');
 
     var max = Math.floor(that.mutationRate);
     if (max === that.mutationMax) {
@@ -88,12 +49,6 @@ function Brain(inputSize, outputSize, layers) {
     console.log(that.mutationRate);
     mutate(0, that.mutationRate, that);
     return that;
-
-    //console.log('Mutation complete.');
-    //console.timeEnd('mutation');
-
-    //console.timeEnd('generation');
-
   };
   for (var i1 = 0; i1 < layers; i1++) {
     var layer = [];
@@ -106,14 +61,10 @@ function Brain(inputSize, outputSize, layers) {
       size = Math.round((inputSize + outputSize) / 2);
     }
     for (var i2 = 0; i2 < size; i2++) {
-      //if (i1 == 2) {
-      //  console.log('Uh oh #2.')
-      //}
       new Neuron(this, i1);
     }
   }
   for (var i1 = 0; i1 < layers - 1; i1++) {
-    //console.log('Connecting layer ' + i1 + ' to layer ' + (i1 + 1));
     var layer1 = i1;
     var layer2 = i1 + 1;
     var list = [];
@@ -145,23 +96,17 @@ function Brain(inputSize, outputSize, layers) {
         for (var prop2 in that.globalReferenceNeurons) {
           if (that.globalReferenceNeurons[prop2].layer == layer2) {
             passed++;
-            //console.log('[Connections: ' + Object.keys(that.globalReferenceNeurons[prop1].connections).length + '] / [Weight: ' + that.globalReferenceNeurons[prop1].weight + ']');
             if (Object.keys(that.globalReferenceNeurons[prop1].connections).length <= that.globalReferenceNeurons[prop1].weight) {
               var rand = getRandomInt(0, 3);
               var existing = Object.keys(that.globalReferenceNeurons[prop1].connections).length;
               var weight = that.globalReferenceNeurons[prop1].weight;
               var needed = weight - existing;
               var remaining = total - passed;
-              //console.log('Total: ' + total);
-              //console.log('Passed: ' + passed);
-              //console.log('Remaining: ' + remaining);
-              //console.log('Needed: ' + needed);
               if (needed === remaining || rand === 1) {
                 that.globalReferenceNeurons[prop1].connect(that.globalReferenceNeurons[prop2]);
               }
             }
           }
-          //console.log(layer1 + ' connecting to ' + layer2)
         }
       }
     }
