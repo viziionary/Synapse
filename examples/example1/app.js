ff75e1e4624314bc4033adfa6b2b13fa802b2f8c
viziionary <jonathan_t0dd@yahoo.com>
2017-10-25 18:17:25 -0500

    Load order fix

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -1,28 +1,30 @@
 import Viewer from './viewer';
 import Synapse from '../../source/index';
 
-var canvas = document.getElementById("brain");
-var viewer = new Viewer(canvas, Synapse.brain);
-var counter = 0;
+document.addEventListener("load", function() {
+	var canvas = document.getElementById("brain");
+	var viewer = new Viewer(canvas, Synapse.brain);
+	var counter = 0;
 
-var network = new Synapse(2, 1, (run) => {
+	var network = new Synapse(2, 1, (run) => {
 
-	viewer.render();
+		viewer.render();
 
-	var output1 = run([1, 1])[0];
-	var output2 = 1 - run([1, 0])[0];
-	var output3 = run([0, 0])[0];
-	var total = 3 - (output1 + output2 + output3);
-	counter++;
-	if (counter > 1000) {
-		console.log('Ended without reaching target: ' + total);
-		return false;
-	}
-	if (total < 2.7) {
-		return total;
-	} else {
-		console.log("Done: " + total);
-		return false;
-	}
-});
-network.run();
+		var output1 = run([1, 1])[0];
+		var output2 = 1 - run([1, 0])[0];
+		var output3 = run([0, 0])[0];
+		var total = 3 - (output1 + output2 + output3);
+		counter++;
+		if (counter > 1000) {
+			console.log('Ended without reaching target: ' + total);
+			return false;
+		}
+		if (total < 2.7) {
+			return total;
+		} else {
+			console.log("Done: " + total);
+			return false;
+		}
+	});
+	network.run();
+})
