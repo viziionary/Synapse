a6d23874d136068d810d1edcfc7647b642595356
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-25 13:46:52 -0600

    set output as return of input function

--- examples/example1/app-new.js
+++ examples/example1/app-new.js
@@ -3,8 +3,8 @@ const Brain = Synapse.brain;
 var entity = new Brain(20, 2, 3);
 entity.evolve(function(entity){
   // run user-designed simulation code
-  entity.input([1, 0.5, 0.2, 0, 1 /* ... */ ]);
-  var output = entity.output;
+
+  var output = entity.input([1, 0.5, 0.2, 0, 1 /* ... */ ]);
   var currentScore = entity.score;
   entity.score = 100;
 });
