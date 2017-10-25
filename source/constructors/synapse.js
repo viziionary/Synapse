3ee0642c6d9a580c64ccf5b669b1a96e97d5439a
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-25 15:14:47 -0600

    synapse run function can be passed an integer to run multiple times

--- source/constructors/synapse.js
+++ source/constructors/synapse.js
@@ -1,12 +1,25 @@
 const Brain = require('./brain');
 const clone = require('../functions/clone');
+const isNumeric = require('../functions/isNumeric');
 class Synapse {
   constructor(runFunction, inputSize, outputSize) {
     this.runFunction = runFunction;
     this.brain = new Brain(inputSize, outputSize);
     this.run = this.run.bind(this);
   }
-  run() {
+  run(times){
+    if (times === null) {
+      this.runOnce();
+    } else {
+      if (!isNumeric(times)) {
+        throw new Error('Synapse: Invalid Run Input');
+      }
+      for (var i = 0; i < times; i++) {
+        this.runOnce();
+      }
+    }
+  }
+  runOnce() {
     var child = clone(this.brain, true);
     child.generate();
     var childScore = this.runFunction(child.input);
