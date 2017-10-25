a53c943a055bd309cf885c484e0b3055925703e7
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-25 16:25:14 -0600

    update app.js

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -1,7 +1,12 @@
+var counter = 0;
+
 import Synapse from '../../source/index';
-var network = new Synapse(20,3,(run2)=>{
-  var output = run2([1, 0.5, 0.2, 0, 1 /* ... */ ]);
-  
-  return newScore;
+var network = new Synapse(2,1,(run)=>{
+  var output1 = run([1, 1])[0];
+  var output2 = 1 - run([1, 0])[0];
+  var output3 = run([0, 0])[0];
+  var total = 3 - (output1 + output2 + output3);
+  counter++;
+  return count > 100 || total < 2.7 ? total : false;
 });
 network.run();
