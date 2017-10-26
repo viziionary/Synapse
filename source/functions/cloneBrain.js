a9455c679a0489c91d92e4b428333fe7a4a25dae
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-25 20:26:26 -0600

    require Brain in cloneBrain

--- source/functions/cloneBrain.js
+++ source/functions/cloneBrain.js
@@ -1,9 +1,10 @@
+const Brain = require('../constructors/brain');
 function cloneBrain(brain){
-  var toClone = Object.assign({},brain));
+  var toClone = Object.assign({},brain);
   delete toClone.globalReferenceNeurons;
   delete toClone.globalReferenceConnections;
   clone = new Brain(toClone.inputSize,toClone.outputSize);
-  Object.assign(clone,JSON.parse(JSON.stringify(toClone));
+  Object.assign(clone,JSON.parse(JSON.stringify(toClone)));
   return clone;
 }
 module.exports = cloneBrain;
