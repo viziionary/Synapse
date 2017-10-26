744c813410fa907f73c8ea9d4293efe9e1015883
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-25 20:43:49 -0600

    update cloneBrain and cloneNeuron

--- source/functions/cloneBrain.js
+++ source/functions/cloneBrain.js
@@ -1,10 +1,19 @@
 const Brain = require('../constructors/brain');
+const setPrototypeOf = require('./setprototypeof');
+const cloneNeuron = require('./cloneNeuron');
 function cloneBrain(brain){
   var toClone = Object.assign({},brain);
   delete toClone.globalReferenceNeurons;
   delete toClone.globalReferenceConnections;
-  clone = new Brain(toClone.inputSize,toClone.outputSize);
+  clone = {globalReferenceNeurons:{},globalReferenceConnections:{}};
+  setPrototypeOf(clone,Brain.prototype);
   Object.assign(clone,JSON.parse(JSON.stringify(toClone)));
+  Object.entries(brain.globalReferenceNeurons).forEach((neuronPair)=>{
+    clone.globalReferenceNeurons[neuronPair[0]] = cloneNeuron(neuronPair[1]);
+  });
+  Object.entries(brain.globalReferenceConnections).forEach((connectionPair)=>{
+    clone.globalReferenceNeurons[connectionPair[0]] = cloneNeuron(connectionPair[1]);
+  });
   return clone;
 }
 module.exports = cloneBrain;
--- source/functions/cloneNeuron.js
+++ source/functions/cloneNeuron.js
@@ -1,4 +1,12 @@
-function cloneNeuron(){
-
+const Neuron = require('../constructors/neuron');
+const setPrototypeOf = require('./setprototypeof');
+function cloneNeuron(neuron){
+  var toClone = Object.assign({},neuron);
+  delete toClone.connections;
+  delete toClone.connected;
+  var clone = {connections:{},connected:{}};
+  Object.assign(clone,toClone);
+  setPrototypeOf(clone,Neuron.prototype);
+  return clone;
 }
 module.exports = cloneNeuron;
