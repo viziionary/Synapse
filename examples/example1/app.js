955aa4cdf13380b8210e2b8868f57cbc9e9482b2
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-26 00:43:50 -0600

    show brain not child

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -9,12 +9,12 @@ window.addEventListener("load", function() {
 	var counter = 0;
 
 	var network = new Synapse(2, 1, (run) => {
-		viewer.render(network.child);
+		viewer.render(network.brain);
 		var output1 = run([1, 1])[0];
 		var output2 = 1 - run([1, 0])[0];
 		var output3 = run([0, 0])[0];
 		var total = 3 - (output1 + output2 + output3);
-		
+
 		if (counter % 10 == 0){
 			console.log(total);
 		}
