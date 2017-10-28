831e7b53da5992cafa0cf48babef79ca2fe767dc
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-27 18:40:58 -0600

    make evolution.simulate async in app.js

--- examples/example2/app.js
+++ examples/example2/app.js
@@ -8,10 +8,10 @@ window.addEventListener("load", function() {
 	var viewer;
 	var counter = 0;
 
-	var network = new Synapse(20, 2, (run) => {
+	var network = new Synapse(20, 2, async (run) => {
 		viewer.render(network.child);
 		var evolution = new Evolution(network, 1, 0, 10000);
-		var score = evolution.simulate(network);
+		var score = await evolution.simulate(network);
 		console.log('Score final', score);
 		counter++;
 		if (counter % 10 == 0) {
