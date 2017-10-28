f711baf613ce450e6d07487f61d33c59a3fe0d9d
viziionary <jonathan_t0dd@yahoo.com>
2017-10-28 02:05:41 -0500

    Fix

--- source/constructors/synapse.js
+++ source/constructors/synapse.js
@@ -9,58 +9,53 @@ class Synapse {
     this.run = this.run.bind(this);
     this.getScoredChild = this.getScoredChild.bind(this);
   }
-  async getScoredChild(){
+  async getScoredChild() {
     var child = cloneBrain(this.brain);
     var childScore;
     childScore = this.runFunction(child.input);
     while (childScore instanceof Promise) {
       childScore = await childScore;
     }
-    return [child,childScore];
+    return [child, childScore];
   }
   async run() {
     //console.log('score',this.brain.score);
-    var chosenOne;
-    var children = {};
-    for (let i = 0; i < 1000; i++) {
-      var child = cloneBrain(this.brain);
-      child.generate();
-      console.log('Debug 1');
-      var childScore = this.runFunction(child.input);
-      console.log('Debug 2');
-      if (childScore instanceof Promise){
-        console.log('Debug 3');
-        childScore = await childScore;
-        console.log('Debug 4');
-        children[score] = child;
-        //console.log(childScore);
+
+    if (this.child) {
+      this.child = cloneBrain(this.brain);
+      this.child.generate()
+    } else {
+      for (let i = 0; i < 1000; i++) {
+        var childData = getScoredChild();
+        var child = childData[0]
+        var childScore = [1]
+        children[childScore] = child;
       }
+      this.child = Object.keys(children).sort((a, b) => {
+        return a > b ? 1 : -1
+      })[0];
     }
 
-    this.child = Object.keys(children).sort((a,b)=>{return a>b ? 1 : -1});
-    console.log('Found the chosen one!', this.child);
-    //console.log(child);
-    //console.log(child.counter);
-    var childScore = this.runFunction(child.input);
-    if (childScore instanceof Promise){
+    var childScore = this.runFunction(this.child.input);
+    if (childScore instanceof Promise) {
       childScore = await childScore;
       //console.log(childScore);
     }
-    child.score = childScore;
+    this.child.score = childScore;
     if (childScore === false) {
       return this.brain;
-    } else if (childScore instanceof Brain){
+    } else if (childScore instanceof Brain) {
       this.brain = childScore;
       return this.run();
     } else {
       if (this.brain.score) {
         if (this.brain.score < childScore) {
-          this.brain = child;
-          console.log('EVOLVED from ' + this.brain.score + ' to ' + child.score);
+          this.brain = this.child;
+          console.log('EVOLVED from ' + this.brain.score + ' to ' + this.child.score);
         }
       } else {
         console.log('Brain born with score of ' + child.score);
-        this.brain = child;
+        this.brain = this.child;
       }
       return this.run();
     }
