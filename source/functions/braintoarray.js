3f9b1a4efbca6a75375cf07248be69e9036ef064
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-27 20:19:30 -0600

    update braintoarray

--- source/functions/braintoarray.js
+++ source/functions/braintoarray.js
@@ -2,25 +2,28 @@ const connectionToArray = require('./connectionToArray');
 function brainToArray(brain){
   var neuronIdConversionMap = {}
   console.log('brain to convert',brain);
+  var connections = [];
   var neurons = brain.structure.map((layer,layerIndex)=>{
     return Object.entries(layer).map((neuronPair,neuronIndex)=>{
       let neuron = neuronPair[1];
       console.log('id:',neuron.id);
       neuronIdConversionMap[neuron.id] = [layerIndex,neuronIndex];
-      return [[],neuron.weight,neuron.memory,neuron.polarization];
+      return [neuron.weight,neuron.memory,neuron.polarization];
     });
   });
   Object.values(brain.globalReferenceConnections).forEach(connection=>{
     let parentLocation = neuronIdConversionMap[connection.source.id];
     let childLocation = neuronIdConversionMap[connection.target.id];
     if (parentLocation && childLocation) {
-      let parent = neurons[parentLocation[0]][parentLocation[1]];
-      parent[0].push([childLocation,connectionToArray(connection)]);
+      if (!connections[parentLocation[0]]) {
+        connections[parentLocation[0]] = [];
+      }
+      connections[parentLocation[0]][parentLocation[1]] = [childLocation,connectionToArray(connection)];
     } else {
       console.log(parentLocation,childLocation);
       console.log('Synapse: convert brain to array error, connection parent child mismatch');
     }
   });
-  return neurons;
+  return [neurons,connections];
 }
 module.exports = brainToArray;
