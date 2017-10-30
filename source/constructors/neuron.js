b6d1f452001768aee0b2eee2d3d29823e963ed84
viziionary <jonathan_t0dd@yahoo.com>
2017-10-29 22:54:56 -0500

    Fix

--- source/constructors/neuron.js
+++ source/constructors/neuron.js
@@ -10,6 +10,7 @@ class Neuron {
     this.brain = brain;
     this.brain.counter++;
     this.brain.globalReferenceNeurons[this.brain.counter] = this;
+    this.brain.layers[type][this.brain.counter] = this;
     this.active = true;
     this.id = brain.counter;
     this.weight = 2;
