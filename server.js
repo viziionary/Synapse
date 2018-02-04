f6fcaf142b19ef6d78b239c52e3218860968bf6a
viziionary <jonathan_t0dd@yahoo.com>
2018-02-03 23:37:00 -0600

    new server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
- var express = require("express");
- var app = express();
- app.get("/", function(req, res) {
-    res.sendfile('index.html')
- });
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]); 
- });
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+// index.js
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
